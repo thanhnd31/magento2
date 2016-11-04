cc7ebdb650774e744427ab65fbb09465601f9b08
thanhnd1 <thanhnd1@smartosc.com>
2016-11-04 16:48:09 +0700

    Add ajax cart module

---
+++ app/code/SMTraining/AjaxCart/view/frontend/web/requirejs-config.js
@@ -0,0 +1,15 @@
+/**
+ * Created by thanhnd1 on 04/11/2016.
+ */
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+var config = {
+    map: {
+        '*': {
+            catalogAddToCart:'SMTraining_AjaxCart/js/dt-add-to-cart'
+        }
+    }
+};
