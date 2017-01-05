1a0a5979c4f3a8a8882cb749d069e50fcedc89e9
Nguyen Duy Thanh <thanhnd1@smartosc.com>
2017-01-05 12:08:51 +0700

    Add add to cart ajax

---
+++ app/code/ThanhND/AjaxCart/view/frontend/requirejs-config.js
@@ -0,0 +1,12 @@
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+var config = {
+    map: {
+        '*': {
+            catalogAddToCart:'ThanhND_AjaxCart/js/dt-add-to-cart'
+        }
+    }
+};
